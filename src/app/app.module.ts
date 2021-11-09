8655c84aedbaa5f79ee46d1d429fac95287625af
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-11-09 17:29:45 +0900

    Add user module dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './modules/users/entities/user.entity';
+import { UserModule } from './modules/users/user.module';
 
 @Module({
   imports: [
@@ -15,9 +17,11 @@ import { AppService } from './app.service';
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
+        entities: [User],
         synchronize: true,
       }),
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
