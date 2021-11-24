bd87e213183eb37e49923d98c54f222558457db4
ksh <soohyeok8901@gmail.com>
2021-11-24 18:16:48 +0900

    Update e2e test

--- test/registration.e2e-spec.ts
+++ test/registration.e2e-spec.ts
@@ -139,85 +139,6 @@ describe('e2e test', () => {
       expect(res.body.message).toEqual('이미 가입된 이메일입니다.');
       expect(res.body.error).toEqual('Conflict');
   });
-
-  it('/auth/signin (POST) expect bad request error (wrong email validation)', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signin')
-    .send({ email: 'this is email', password: 'password1' })
-    
-    expect(res.statusCode).toBe(400);
-    expect(res.body.error).toEqual("Bad Request");
-  });
-
-  
-  it('/auth/signin (POST) expect bad request error (only number password)', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signin')
-    .send({ email: 'email@email.com', password: '12345678' })
-    
-    expect(res.statusCode).toBe(400);
-    expect(res.body.error).toEqual("Bad Request");
-  });
-  
-  it('/auth/signin (POST) expect bad request error (only string password)', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signin')
-    .send({ email: 'email@email.com', password: 'abcdefgh' })
-    
-    expect(res.statusCode).toBe(400);
-    expect(res.body.error).toEqual("Bad Request");
-  });
-  
-  it('/auth/signin (POST) expect bad request error (too short password)', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signin')
-    .send({ email: 'email@email.com', password: 'pw1' })
-    
-    expect(res.statusCode).toBe(400);
-    expect(res.body.error).toEqual("Bad Request");
-  });
-  
-  it('/auth/signin (POST) expect bad request error (too long password)', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signin')
-    .send({ email: 'email@email.com', password: 'password1password1password1password1password1password1' })
-    
-    expect(res.statusCode).toBe(400);
-    expect(res.body.error).toEqual("Bad Request");
-  });
-  
-  it('/auth/signin (POST) expect Unauthorized error (wrong password)', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signin')
-    .send({ email: 'email@email.com', password: 'password2' })
-    
-    expect(res.statusCode).toBe(401);
-    expect(res.body.error).toEqual("Unauthorized");
-  });
-  
-  it('/auth/signin (POST) should return JSON included accessToken and refreshToken', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signin')
-    .send({ email: 'email@email.com', password: 'password1' })
-
-    accessToken = res.body.accessToken;
-    refreshToken = res.body.refreshToken;
-    
-    expect(res.statusCode).toBe(200);
-    expect(res.body.accessToken).toBeDefined();
-    expect(res.body.refreshToken).toBeDefined();
-  });
-
-  it('/auth/signOut (POST) should return response.status success', async () => {
-    const res =  await request(app.getHttpServer())
-    .post('/auth/signout')
-    .set('Authorization', `Bearer ${accessToken}`)
-    .send({ email: 'email@email.com', password: 'password1' })
-    
-    expect(res.statusCode).toBe(204);
-  });
-
-
 })
 
 /***
@@ -226,9 +147,5 @@ describe('e2e test', () => {
 회원가입 실패 (잘못된 패스워드) ㅇ
 회원가입 성공 ㅇ
 중복 회원가입 시도 ㅇ
-로그인 실패 (잘못된 이메일) ㅇ
-로그인 실패 (잘못된 패스워드) ㅇ
-로그인 실패 (다른 패스워드) ㅇ
-로그인 성공 (토큰반환) ㅇ
-로그아웃 성공 (user DB refreshToken null로 변경)
  */
+
