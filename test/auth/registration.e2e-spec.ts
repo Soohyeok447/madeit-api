3b8948b40b68870fc1ce0a18dd78d65cc8ff4a8d
ksh <soohyeok8901@gmail.com>
2021-11-24 20:37:10 +0900

    Fix directory

--- test/auth/registration.e2e-spec.ts
+++ test/auth/registration.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from '../src/app/app.module';
-import { UserRepository } from '../src/app/modules/users/users.repository';
+import { AppModule } from '../../src/app/app.module';
+import { UserRepository } from '../../src/app/modules/users/users.repository';
 import * as request from 'supertest';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
