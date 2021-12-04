c779dbdeee9cd9dbc4b165db3eca87f8008d1e1c
ksh <soohyeok8901@gmail.com>
2021-12-04 17:02:03 +0900

    Add googleOauth service interface
    google Oauth 3rd party 서비스를 위한 인터페이스, 테스트를 생성했습니다.
    인자로는 googleUserProfile<GoogleUserProfile>을 받습니다.
    googleUserProfile은 googleOauth strategy를 통해 얻는 'profile' 객체를
    필요한 것만 뽑아서 만든 객체 입니다.
    { email, email_verified, locale, provider } 가 있습니다
    이 파라미터의 존재이유는 만일 user를 find후, DB에 존재하지않아서
    registration을 해야 할 경우, 가입에 도움이 될법한 data들을
    그대로 받아 사용할 수 있게하기 위함입니다.
    그렇지만 이후 registration user onboarding 정책이 변하게 되고
    확정이 된다면 언제든 이 type GoogleUserProfile 구조는
    삭제될 수도 수정될 수도 있습니다.
    
    'profile'에는 구글에서 제공하는
    user infomation( email, locale, name 등의) 객체가 있습니다.

---
+++ src/app/common/types/google_sign_in.type.ts
@@ -0,0 +1,9 @@
+export type GoogleUserProfile = {
+  provider: string;
+
+  email_verified: boolean;
+
+  email: string;
+  
+  locale: string;
+}
--- src/app/modules/auth/interfaces/auth.service.ts
+++ src/app/modules/auth/interfaces/auth.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { AuthCredentialInput, AuthCredentialOutput } from '../dto/auth_credential.dto';
+import { GoogleOauthOutput } from '../dto/google_oauth.dto';
 import { RefreshOutput } from '../dto/refresh.dto';
+import { GoogleUserProfile } from '../../../common/types/google_sign_in.type';
 
 @Injectable()
 export abstract class AuthService {
 
-  public abstract signIn({ email, password }: AuthCredentialInput): Promise<AuthCredentialOutput>
+  public abstract signIn({ email, password }: AuthCredentialInput): Promise<AuthCredentialOutput>;
 
-  public abstract signOut(id: string): Promise<void>
+  public abstract signOut(id: string): Promise<void>;
 
-  public abstract reissueAccessToken(refreshToken: string, id: number): Promise<RefreshOutput>
+  public abstract reissueAccessToken(refreshToken: string, id: number): Promise<RefreshOutput>;
+
+  public abstract googleSignIn(googleUserProfile: GoogleUserProfile): Promise<GoogleOauthOutput>;
 }
-
-
