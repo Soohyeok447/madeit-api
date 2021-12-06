786015f56a2b834176b5550c4e04153ca30d998f
ksh <soohyeok8901@gmail.com>
2021-12-06 21:16:21 +0900

    Add googleAuth entry point and Create Dto for googleAuth

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, HttpCode, Post, Req, Headers, UseGuards, Get, Param, Header } from '@nestjs/common';
 import { User } from './decorators/user.decorator';
-import { AuthCredentialInput, AuthCredentialOutput } from './dto/auth_credential.dto';
 import { GoogleOauthInput, GoogleOauthOutput } from './dto/google_oauth.dto';
 import { RefreshOutput } from './dto/refresh.dto';
 import { GoogleOauthGuard } from './guards/auth.guard';
@@ -14,6 +13,21 @@ export class AuthController {
   constructor(
     private readonly authService: AuthService,
   ) { }
+
+
+  //구글 로그인
+  @Post('google')
+  async googleAuth(
+    @Body() googleOauthInput: GoogleOauthInput,
+  ): Promise<GoogleOauthOutput> {
+    const { accessToken, refreshToken } = await this.authService.googleAuth(googleOauthInput);
+
+    return {
+      accessToken,
+      refreshToken
+    }
+  }
+
 
   //user DB에 접근해서 refreshToken을 지워줍니다.
   @Post('signout')
@@ -29,9 +43,9 @@ export class AuthController {
   async reissueAccessToken(@Headers() headers, @User() user): Promise<RefreshOutput> {
     const refreshToken = headers.authorization.split(' ')[1];
 
-    const result = await this.authService.reissueAccessToken(refreshToken, user.id);
+    const { accessToken } = await this.authService.reissueAccessToken(refreshToken, user.id);
 
-    return { accessToken: result.accessToken };
+    return { accessToken };
   }
 
   @Post('test')
--- src/app/modules/auth/dto/google_oauth.dto.ts
+++ src/app/modules/auth/dto/google_oauth.dto.ts
@@ -1,23 +1,12 @@
-import { GoogleUserProfile } from "../../../common/types/google_sign_in.type";
+import { IsString } from "class-validator";
+
+export class GoogleOauthInput {
+  @IsString()
+  googleAccessToken: string;
+}
+
 
 export class GoogleOauthOutput {
-
-  /**
-   * it will be decided from exist of user.
-   * if user exist, result will return true. another case is false
-   * if is it false, client should redirect to signUp page 
-   * and call create method including signup form 
-   */
-  isExist: boolean;
-
-  /**
-   * it will be return if result state is false.
-   * this parameter has helpful user data object for registration
-   * this data object's structure should be modify until the format of the registration form is decided
-   */
-  user?: GoogleUserProfile;
-
-
   /**
    * both of them will return if user is exist
    */
