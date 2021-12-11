959d33a9fd226f3b2d82fe02ed40ce80cbbfa740
ksh <soohyeok8901@gmail.com>
2021-12-11 16:38:11 +0900

    Move concrete repository to infrastructure layer

--- src/infrastructure/repositories/users.repository.ts
+++ src/infrastructure/repositories/users.repository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Injectable } from "@nestjs/common";
-import { User } from "./entities/user.entity";
+import { User } from "../entities/user.entity";
 import { hash } from "src/app/common/util/util";
 
 @Injectable()
