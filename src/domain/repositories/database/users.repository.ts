580829cbf38b1fa55915beaaafa463c376796727
ksh <soohyeok8901@gmail.com>
2021-12-12 15:06:16 +0900

    Fix findOneByUserId parameter type to string

--- src/domain/repositories/database/users.repository.ts
+++ src/domain/repositories/database/users.repository.ts
@@ -3,7 +3,7 @@ import { UserModel } from '../../models/user.model';
 export abstract class UserRepository {
   abstract findOne(id: number): Promise<UserModel>;
 
-  abstract findOneByUserId(userId: number): Promise<UserModel>;
+  abstract findOneByUserId(userId: string): Promise<UserModel>;
 
   abstract findOneByEmail(email: string): Promise<UserModel>;
 
