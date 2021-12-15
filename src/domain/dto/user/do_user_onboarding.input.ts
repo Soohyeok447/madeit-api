7f8c91e4b3ef3f966b824f0cc8438ec756fe1ba2
ksh <soohyeok8901@gmail.com>
2021-12-15 18:46:04 +0900

    Add DoUserOnboardingInput dto to transport adapter to service

---
+++ src/domain/dto/user/do_user_onboarding.input.ts
@@ -0,0 +1,14 @@
+import { Gender } from "src/domain/models/enum/gender.enum";
+import { Job } from "src/domain/models/enum/job.enum";
+
+export class DoUserOnboardingInput {
+  id: string; // this is primary key in user table got from user decorator
+
+  username: string;
+
+  birth: string;
+
+  job: Job;
+
+  gender: Gender;
+}
