c40ff6f8afd523353d31a8e7155a5f94095a535c
ksh <soohyeok8901@gmail.com>
2021-12-15 18:37:56 +0900

    Delete useless property in createuserdto

--- src/domain/repositories/dto/user/create.dto.ts
+++ src/domain/repositories/dto/user/create.dto.ts
@@ -1,18 +1,9 @@
-import { Gender } from "src/domain/models/enum/gender.enum";
-import { Job } from "src/domain/models/enum/job.enum";
-
 export class CreateUserDto {
   public user_id: string;
 
   public email?: string;
 
   public username?: string;
-
-  public gender?: Gender;
-
-  public job?: Job;
-
-  public birth?: string;
 
   public provider: string;
 }
