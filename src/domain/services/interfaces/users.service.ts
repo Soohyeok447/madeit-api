bf578487c346ad6dc738f267c1fe1292bdff58b6
ksh <soohyeok8901@gmail.com>
2021-12-15 20:44:25 +0900

    Implement findUser()

--- src/domain/services/interfaces/users.service.ts
+++ src/domain/services/interfaces/users.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { DoUserOnboardingInput } from 'src/domain/dto/user/do_user_onboarding.input';
+import { FindUserInput } from 'src/domain/dto/user/find_user.input';
+import { FindUserOutput } from 'src/domain/dto/user/find_user.output';
 
 @Injectable()
 export abstract class UsersService {
   public abstract doUserOnboarding({ id, birth, gender, job, username }: DoUserOnboardingInput): Promise<void>;
 
-  public abstract find({ id }: FindUserInput): Promise<FindUserOutput>;
+  public abstract findUser({ id }: FindUserInput): Promise<FindUserOutput>;
 }
--- src/domain/services/users.service.ts
+++ src/domain/services/users.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UserNotFoundException } from 'src/domain/exceptions/users/user_not_found.exception';
 import { DoUserOnboardingInput } from '../dto/user/do_user_onboarding.input';
+import { FindUserInput } from '../dto/user/find_user.input';
+import { FindUserOutput } from '../dto/user/find_user.output';
 import { UsernameConflictException } from '../exceptions/users/username_conflict.exception';
 import { UserNotRegisteredException } from '../exceptions/users/user_not_registered.exception';
+import { UserModel } from '../models/user.model';
 import { UserRepository } from '../repositories/users.repository';
 import { UsersService } from './interfaces/users.service';
 
@@ -29,6 +32,20 @@ export class UsersServiceImpl extends UsersService {
     }
 
     await this.userRespository.update(id, onboardingData);
+  }
+
+  public async findUser({ id }: FindUserInput): Promise<FindUserOutput> {
+    const user: UserModel = await this.userRespository.findOne(id);
+    
+    if(!user.birth || !user.gender || !user.job || !user.username){
+      throw new UserNotRegisteredException();
+    }
+
+    const output :FindUserOutput = {
+      ...user
+    }
+
+    return output;
   }
 }
 
