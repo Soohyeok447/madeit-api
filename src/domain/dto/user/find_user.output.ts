cd0bf145f85b142400bae90359317e5ab683a22f
ksh <soohyeok8901@gmail.com>
2021-12-15 20:44:03 +0900

    Add find_user input output dto

---
+++ src/domain/dto/user/find_user.output.ts
@@ -0,0 +1,16 @@
+import { Gender } from "src/domain/models/enum/gender.enum";
+import { Job } from "src/domain/models/enum/job.enum";
+
+export class FindUserOutput {
+  email?: string;
+
+  username?: string;
+
+  provider: string;
+
+  birth?: string;
+
+  gender?: Gender;
+
+  job?: Job;
+}
