6e5b26263f198b555599688b2c206f3bf842640c
ksh <soohyeok8901@gmail.com>
2021-12-15 18:17:37 +0900

    Add comment for dev

--- src/domain/exceptions/users/user_not_registered_exception.ts
+++ src/domain/exceptions/users/user_not_registered_exception.ts
@@ -1,5 +1,8 @@
 import { ServiceUnavailableException } from '@nestjs/common';
 
+/**
+ * 분명 Jwt토큰 까서 id를 받았는데 찾은 user의 row가 완벽하지 않다? => 등록 필요
+ */
 export class UserNotRegisteredException extends ServiceUnavailableException {
   constructor() {
     super(`유저 등록이 필요합니다.`);
