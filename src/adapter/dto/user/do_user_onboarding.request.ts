862d49a3c1f74d00033cfeab1675e21c6485c871
ksh <soohyeok8901@gmail.com>
2021-12-15 19:10:28 +0900

    Add class-validator in request dto

--- src/adapter/dto/user/do_user_onboarding.request.ts
+++ src/adapter/dto/user/do_user_onboarding.request.ts
@@ -1,12 +1,17 @@
+import { IsEnum, IsString } from "class-validator";
 import { Gender } from "src/domain/models/enum/gender.enum";
 import { Job } from "src/domain/models/enum/job.enum";
 
 export class DoUserOnboardingRequest {
+  @IsString()
   username: string;
 
+  @IsString()
   birth: string;
 
+  @IsEnum(Job)
   job: Job;
 
+  @IsEnum(Gender)
   gender: Gender;
 }
