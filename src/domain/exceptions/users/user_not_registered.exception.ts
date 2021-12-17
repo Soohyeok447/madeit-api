9f4b00ce26f46d0d75b71374a5385a3d37c75c81
ksh <soohyeok8901@gmail.com>
2021-12-17 17:46:37 +0900

    Fix exception extends forbiddenException

--- src/domain/exceptions/users/user_not_registered.exception.ts
+++ src/domain/exceptions/users/user_not_registered.exception.ts
@@ -1,9 +1,9 @@
-import { ServiceUnavailableException } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common';
 
 /**
  * 분명 Jwt토큰 까서 id를 받았는데 찾은 user의 row가 완벽하지 않다? => 등록 필요
  */
-export class UserNotRegisteredException extends ServiceUnavailableException {
+export class UserNotRegisteredException extends ForbiddenException {
   constructor() {
     super(`유저 등록이 필요합니다.`);
   }
