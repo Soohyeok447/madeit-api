637dd1d5952849699ed0667aa5cd34166a71f09c
ksh <soohyeok8901@gmail.com>
2022-01-04 21:09:07 +0900

    Add update service in alarm context

--- src/infrastructure/repositories/alarm.repository.ts
+++ src/infrastructure/repositories/alarm.repository.ts
@@ -9,6 +9,7 @@ import { UpdateDto } from 'src/domain/repositories/dto/alarm/update.dto';
 import { ConflictAlarmException } from '../exceptions/conflict_alarm.exception';
 import { Day } from 'src/domain/models/enum/day.enum';
 import { AlarmValidationException } from '../exceptions/alarm_validation.exception';
+import { AlarmNotFoundException } from '../exceptions/alarm_not_found.exception';
 
 @Injectable()
 export class AlarmRepositoryImpl implements AlarmRepository {
@@ -35,15 +36,14 @@ export class AlarmRepositoryImpl implements AlarmRepository {
 
             assertResult = true;
           }
-        })
+        });
 
-      })
-    })
+      });
+    });
 
     if (assertResult) {
       throw new ConflictAlarmException(`[${conflictDay}] ${data.time} 중복`);
     }
-
     const newAlarm = new this.alarmModel(data);
 
     try {
@@ -54,23 +54,74 @@ export class AlarmRepositoryImpl implements AlarmRepository {
     }
   }
 
-  update(userId: string, alarmId: string, data: UpdateDto): Promise<void> {
-    throw new Error('Method not implemented.');
+  public async update(userId:string, alarmId: string, data: UpdateDto): Promise<void> {
+    const foundAlarm = await this.alarmModel.findById(alarmId);
+
+    if(!foundAlarm){
+      throw new AlarmNotFoundException('알람이 없음');
+    }
+
+    let alarms = await this.alarmModel.find({ user_id: userId });
+
+    //현재 수정중인 알람 중복체크에서 제거
+    const index = alarms.findIndex(e=> e["_id"] == alarmId);
+
+    alarms.splice(index,1);
+
+    //중복된 요일
+    let conflictDay: Day[] = [];
+
+    //중복검사 결과
+    let assertResult: boolean;
+
+    //각각의 요일에 대한 시간 중복검사
+    data.day.forEach(day => {
+      alarms.find(alarm => {
+        alarm.day.find(e => {
+          if (e == day && alarm.time == data.time) {
+            conflictDay.push(e);
+
+            assertResult = true;
+          }
+        });
+
+      });
+    });
+
+    if (assertResult) {
+      throw new ConflictAlarmException(`[${conflictDay}] ${data.time} 중복`);
+    }    
+    
+    foundAlarm.alias = data.alias;
+    foundAlarm.time = data.time;
+    foundAlarm.day = data.day;
+    foundAlarm.routineId = data.routineId;
+
+    await foundAlarm.save();
   }
 
   delete(userId: string, alarmId: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
-  findAll(userId: string): Promise<Alarm[]> {
-    throw new Error('Method not implemented.');
+  public async findAll(userId: string): Promise<Alarm[]> {
+    const alarms = await this.alarmModel.find({user_id: userId});
+
+    if(!alarms){
+      return undefined;
+    }
+
+    return alarms;
   }
 
-  findOne(userId: string, alarmId: string): Promise<Alarm> {
-    throw new Error('Method not implemented.');
+  public async findOne(alarmId: string): Promise<Alarm> {
+    const alarm = await this.alarmModel.findById(alarmId);
+
+    if(!alarm){
+      return undefined;
+    }
+
+    return alarm;
   }
-
-
-
 
 }
