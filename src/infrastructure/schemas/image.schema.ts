b63770caf57e1c286716e2c5e848665d966479c9
ksh <soohyeok8901@gmail.com>
2022-01-09 04:05:24 +0900

    Fix image schema

--- src/infrastructure/schemas/image.schema.ts
+++ src/infrastructure/schemas/image.schema.ts
@@ -5,14 +5,14 @@ import { Image } from 'src/domain/common/enums/image.enum';
 //하면서 계속 수정;
 export const ImageSchema = new mongoose.Schema(
   {
+    //이미지 타입
+    type: {
+      type: Image,
+      required: true,
+    },
+
     //이미지 리스트
     image: [{
-      //이미지 타입
-      type: {
-        type: Image,
-        required: true,
-      },
-
       //디바이스 타입
       device: {
         type: String,
