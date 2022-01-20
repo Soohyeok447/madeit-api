d2cea5bf3f0a5bc422bacb24db6e9cf401b4c933
ksh <soohyeok8901@gmail.com>
2022-01-21 07:45:39 +0900

    Fix image provider

--- src/domain/common/providers/image.provider.ts
+++ src/domain/common/providers/image.provider.ts
@@ -1,9 +1,15 @@
+import { File } from "src/domain/types";
+import { ImageType } from "../enums/image.enum";
 import { Resolution } from "../enums/resolution.enum";
+import { Image } from "../models/image.model";
+import { S3Object } from "../models/s3object.model";
 
 
 export abstract class ImageProvider {
-  //쿼리, 키, filename을 조합해서 cdn url로 이미지 요청을 보냄
-  abstract requestImageToCloudfront(resolution: Resolution, key: string, filenames: string[]):  Promise<string | string[]>;
+  //쿼리, 키, filename을 조합해서 cdn url로 이미지 요청을 A보냄
+  abstract requestImageToCloudfront(resolution: Resolution, imageModel: Image): Promise<string | string[]>;
 
+  abstract mapDocumentToImageModel(imageDocument: { [key: string]: any });
 
+  abstract putImageToS3(thumbnail: File, key: string);
 }
--- src/infrastructure/providers/image.provider.ts
+++ src/infrastructure/providers/image.provider.ts
@@ -1,43 +1,96 @@
+import { ImageType } from "src/domain/common/enums/image.enum";
 import { Resolution } from "src/domain/common/enums/resolution.enum";
+import { Image } from "src/domain/common/models/image.model";
+import { S3Object } from "src/domain/common/models/s3object.model";
 import { ImageProvider } from "src/domain/common/providers/image.provider";
+import { File } from "src/domain/types";
+import { s3 } from "../config/s3";
+import { getS3BucketName } from "../environment";
 import { HttpClientImpl } from "../utils/providers/http_client";
 
 
 export class ImageProviderImpl implements ImageProvider {
-  public async requestImageToCloudfront(resolution: Resolution, key: string, filenames: string[]): Promise<string | string[]> {
+
+  //imageModel mapper
+  public mapDocumentToImageModel(imageDocument: { [key: string]: any }) {
+    const imageModel: Image = {
+      id: imageDocument["_id"],
+      type: imageDocument["type"],
+      referenceId: imageDocument["reference_id"],
+      referenceModel: imageDocument["reference_model"],
+      key: imageDocument["key"],
+      filenames: imageDocument["filenames"],
+    }
+
+    return imageModel;
+  }
+
+  public putImageToS3(imageFile: File, key: string) {
+    const Params = {
+      Bucket: getS3BucketName(),
+      Key: `origin/${key}/${imageFile.originalname}`,
+      Body: imageFile.buffer,
+      ContentType: "image",
+    };
+
+    let result;
+
+    new Promise((resolve, reject) => {
+      result = s3.putObject(Params, (err, data) => {
+        if (err)
+          reject;
+
+        resolve(data);
+      });
+    });
+
+    return result;
+  }
+
+
+
+  public async requestImageToCloudfront(
+    resolution: Resolution,
+    imageModel: Image
+  ): Promise<string | string[]> {
     const baseUrl = `http://d28okinpr57gbg.cloudfront.net`;
 
+    const filenames = imageModel["filenames"];
+    const key = imageModel["key"];
+
+    //single image file
     if (filenames.length === 1) {
       const url = `${baseUrl}/${resolution}/${key}/${filenames[0]}`;
 
-      try{
-        const result = await new HttpClientImpl().get(encodeURI(url));
-  
-        const thumbnail = Buffer.from(result.data, 'base64').toString('hex');
-        // const image = Buffer.from(result.data, 'base64').toString('utf8');
-  
-        return thumbnail;
-      }catch(err){
-        console.log(err);
+      let result;
 
-        throw new Error('thumbnail 로딩 에러');
+      try {
+        result = await new HttpClientImpl().get(url);
+
+      } catch (err) {
+        throw new Error('image 로딩 에러');
       }
+      const thumbnail = Buffer.from(result.data, 'base64').toString('hex');
+      // const image = Buffer.from(result.data, 'base64').toString('utf8');
+
+      return thumbnail;
     }
-    
+
+    //multiple image files
     const cardnews = await Promise.all(filenames.map(async e => {
       const url = `${baseUrl}/${resolution}/${key}/${e}`;
-      
-      try{
-        const result = await new HttpClientImpl().get(encodeURI(url));
-        
-        const eachCardnews = Buffer.from(result.data, 'base64').toString('hex');
-        
-        return eachCardnews;
-      }catch(err){
-        console.log(err);
-        
+
+      let result;
+
+      try {
+        result = await new HttpClientImpl().get(encodeURI(url));
+
+      } catch (err) {
         throw new Error('cardnews 로딩 에러');
       }
+      const eachCardnews = Buffer.from(result.data, 'base64').toString('hex');
+
+      return eachCardnews;
     }))
 
     return cardnews;
