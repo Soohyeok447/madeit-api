7ea0578bb11a42767a0c8b21f960c649b82d6cd1
ksh <soohyeok8901@gmail.com>
2022-02-14 23:52:21 +0900

    Rename signinusecase

--- src/domain/use-cases/auth/sign-in/SignInUseCaseasdf.ts
+++
@@ -1,9 +0,0 @@
-import { UseCase } from '../../UseCase';
-import { SignInResonse } from '../response.index';
-import { SignInUseCaseParams } from './dtos/SignInUseCaseParams';
-
-export abstract class SignInUseCase
-  implements UseCase<SignInUseCaseParams, SignInResonse>
-{
-  abstract execute(params: SignInUseCaseParams): SignInResonse;
-}
