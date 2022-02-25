3538f023b7b346689beee51f3419733e82b41467
ksh <soohyeok8901@gmail.com>
2022-02-26 05:51:00 +0900

    Comment cart e2e tests

--- test/e2e/cart/addRoutinesToCart.e2e-spec.ts
+++ test/e2e/cart/addRoutinesToCart.e2e-spec.ts
@@ -1,190 +1,190 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
-import { setTimeOut } from '../e2e-env';
-import { AppModule } from '../../../src/ioc/AppModule';
-import { DatabaseService } from 'src/ioc/DatabaseModule';
-import { SignInRequestDto } from 'src/adapter/auth/sign-in/SignInRequestDto';
-import { AddRoutineToCartRequestDto } from 'src/adapter/cart/add-routine-to-cart/AddRoutineToCartRequestDto';
-import { AddRoutineRequestDto } from 'src/adapter/routine/add-routine/AddRoutineRequestDto';
-import { Category } from 'src/domain/enums/Category';
-import { RoutineType } from 'src/domain/enums/RoutineType';
-import { onboard, addRoutine, signIn, authorize, addRoutinesToCart } from '../request.index';
+// import { INestApplication, ValidationPipe } from '@nestjs/common';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { setTimeOut } from '../e2e-env';
+// import { AppModule } from '../../../src/ioc/AppModule';
+// import { DatabaseService } from 'src/ioc/DatabaseModule';
+// import { SignInRequestDto } from 'src/adapter/auth/sign-in/SignInRequestDto';
+// import { AddRoutineToCartRequestDto } from 'src/adapter/cart/add-routine-to-cart/AddRoutineToCartRequestDto';
+// import { AddRoutineRequestDto } from 'src/adapter/routine/add-routine/AddRoutineRequestDto';
+// import { Category } from 'src/domain/enums/Category';
+// import { RoutineType } from 'src/domain/enums/RoutineType';
+// import { onboard, addRoutine, signIn, authorize, addRoutinesToCart } from '../request.index';
 
-describe('addRoutinesToCart e2e test', () => {
-  let app: INestApplication;
-  let httpServer: any;
-  let dbConnection;
+// describe('addRoutinesToCart e2e test', () => {
+//   let app: INestApplication;
+//   let httpServer: any;
+//   let dbConnection;
 
-  let accessToken: string;
-  let refreshToken: string;
+//   let accessToken: string;
+//   let refreshToken: string;
 
-  setTimeOut();
+//   setTimeOut();
 
-  beforeAll(async () => {
-    const moduleRef: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+//   beforeAll(async () => {
+//     const moduleRef: TestingModule = await Test.createTestingModule({
+//       imports: [AppModule],
+//     }).compile();
 
-    app = moduleRef.createNestApplication();
+//     app = moduleRef.createNestApplication();
 
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }),
-    );
+//     app.useGlobalPipes(
+//       new ValidationPipe({
+//         whitelist: true,
+//         forbidNonWhitelisted: true,
+//         transform: true,
+//       }),
+//     );
 
-    await app.init();
-    dbConnection = moduleRef.get<DatabaseService>(DatabaseService).getConnection();
-    httpServer = app.getHttpServer();
+//     await app.init();
+//     dbConnection = moduleRef.get<DatabaseService>(DatabaseService).getConnection();
+//     httpServer = app.getHttpServer();
 
-    const signInParam: SignInRequestDto = {
-      thirdPartyAccessToken: 'asdfasdfasdfasdf'
-    }
+//     const signInParam: SignInRequestDto = {
+//       thirdPartyAccessToken: 'asdfasdfasdfasdf'
+//     }
 
-    const res = await signIn(httpServer, signInParam)
+//     const res = await signIn(httpServer, signInParam)
 
-    accessToken = res.body.accessToken;
-    refreshToken = res.body.refreshToken;
+//     accessToken = res.body.accessToken;
+//     refreshToken = res.body.refreshToken;
 
-    const onboardParam = {
-      username: "테스트",
-      birth: "0000-00-00",
-      job: "student",
-      gender: "male"
-    };
+//     const onboardParam = {
+//       username: "테스트",
+//       birth: "0000-00-00",
+//       job: "student",
+//       gender: "male"
+//     };
 
-    await onboard(httpServer, accessToken, onboardParam);
+//     await onboard(httpServer, accessToken, onboardParam);
 
-  });
+//   });
 
-  afterAll(async () => {
-    await dbConnection.collection('users').deleteMany({});
-    await dbConnection.collection('routines').deleteMany({});
-    await dbConnection.collection('carts').deleteMany({});
+//   afterAll(async () => {
+//     await dbConnection.collection('users').deleteMany({});
+//     await dbConnection.collection('routines').deleteMany({});
+//     await dbConnection.collection('carts').deleteMany({});
 
-    await app.close();
-  });
+//     await app.close();
+//   });
 
-  let firstRoutineId: string;
-  let secondRoutineId: string;
+//   let firstRoutineId: string;
+//   let secondRoutineId: string;
 
 
-  describe('POST v1/carts', () => {
-    describe('try add routine to cart', () => {
-      describe('using invalid mongo object id', () => {
-        it('BadRequestException should be thrown', async () => {
-          const addRoutineToCartParams: AddRoutineToCartRequestDto = {
-            routineId: '123'
-          }
+//   describe('POST v1/carts', () => {
+//     describe('try add routine to cart', () => {
+//       describe('using invalid mongo object id', () => {
+//         it('BadRequestException should be thrown', async () => {
+//           const addRoutineToCartParams: AddRoutineToCartRequestDto = {
+//             routineId: '123'
+//           }
 
-          const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams);
+//           const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams);
 
-          expect(res.statusCode).toBe(400);
-        })
-      })
+//           expect(res.statusCode).toBe(400);
+//         })
+//       })
 
-      describe('using unintact request body', () => {
-        it('BadRequestException should be thrown', async () => {
-          const addRoutineToCartParams = {
-            wrongKey: '123456789101112131415161'
-          }
+//       describe('using unintact request body', () => {
+//         it('BadRequestException should be thrown', async () => {
+//           const addRoutineToCartParams = {
+//             wrongKey: '123456789101112131415161'
+//           }
 
-          const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams);
+//           const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams);
 
-          expect(res.statusCode).toBe(400);
-        })
-      })
+//           expect(res.statusCode).toBe(400);
+//         })
+//       })
 
-      describe('using nonexistent routine id', () => {
-        it('RoutineNotFoundException should be thrown', async () => {
-          const addRoutineToCartParams = {
-            routineId: '123456789101112131415161'
-          }
+//       describe('using nonexistent routine id', () => {
+//         it('RoutineNotFoundException should be thrown', async () => {
+//           const addRoutineToCartParams = {
+//             routineId: '123456789101112131415161'
+//           }
 
-          const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams);
+//           const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams);
 
-          expect(res.statusCode).toBe(404);
-        })
-      })
-    })
-  })
+//           expect(res.statusCode).toBe(404);
+//         })
+//       })
+//     })
+//   })
 
-  describe('POST v1/routines', () => {
-    it('add routine two times', async () => {
-      await authorize(httpServer, accessToken);
+//   describe('POST v1/routines', () => {
+//     it('add routine two times', async () => {
+//       await authorize(httpServer, accessToken);
 
-      for (let i = 0; i < 2; i++) {
-        let addRoutineParam: AddRoutineRequestDto = {
-          name: `e2eTEST${i}`,
-          category: Category.Health,
-          type: RoutineType.Embeded,
-          introductionScript: 'e2eTEST',
-          motivation: 'e2eTEST',
-          price: "0"
-        }
+//       for (let i = 0; i < 2; i++) {
+//         let addRoutineParam: AddRoutineRequestDto = {
+//           name: `e2eTEST${i}`,
+//           category: Category.Health,
+//           type: RoutineType.Embeded,
+//           introductionScript: 'e2eTEST',
+//           motivation: 'e2eTEST',
+//           price: "0"
+//         }
 
-        const res = await addRoutine(httpServer, accessToken, addRoutineParam);
+//         const res = await addRoutine(httpServer, accessToken, addRoutineParam);
 
-        if (i === 0) {
-          firstRoutineId = res.body.id;
-        } else {
-          secondRoutineId = res.body.id;
-        }
-      }
-    });
-  })
+//         if (i === 0) {
+//           firstRoutineId = res.body.id;
+//         } else {
+//           secondRoutineId = res.body.id;
+//         }
+//       }
+//     });
+//   })
 
-  describe('POST v1/carts after add routines', () => {
-    describe('try add routine to cart', () => {
-      describe('using intact request body include valid existent routineId', () => {
-        it('success to add routine to cart', async () => {
-          const addRoutineToCartParams = {
-            routineId: firstRoutineId
-          }
+//   describe('POST v1/carts after add routines', () => {
+//     describe('try add routine to cart', () => {
+//       describe('using intact request body include valid existent routineId', () => {
+//         it('success to add routine to cart', async () => {
+//           const addRoutineToCartParams = {
+//             routineId: firstRoutineId
+//           }
 
-          const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams)
+//           const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams)
         
-          expect(res.statusCode).toBe(201);
-        })
-      })
+//           expect(res.statusCode).toBe(201);
+//         })
+//       })
 
-      describe('using intact request body that include duplicated routineId from cart', () => {
-        it('CartConflictException should be thrown', async () => {
-          const addRoutineToCartParams = {
-            routineId: firstRoutineId
-          }
+//       describe('using intact request body that include duplicated routineId from cart', () => {
+//         it('CartConflictException should be thrown', async () => {
+//           const addRoutineToCartParams = {
+//             routineId: firstRoutineId
+//           }
 
-          const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams)
+//           const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams)
         
-          expect(res.statusCode).toBe(409);
-        })
-      })
+//           expect(res.statusCode).toBe(409);
+//         })
+//       })
 
-      describe('using intact request body that include not duplicated routineId from cart', () => {
-        it('success to add routine to cart', async () => {
-          const addRoutineToCartParams = {
-            routineId: secondRoutineId
-          }
+//       describe('using intact request body that include not duplicated routineId from cart', () => {
+//         it('success to add routine to cart', async () => {
+//           const addRoutineToCartParams = {
+//             routineId: secondRoutineId
+//           }
 
-          const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams)
+//           const res = await addRoutinesToCart(httpServer, accessToken, addRoutineToCartParams)
         
-          expect(res.statusCode).toBe(201);
-        })
-      })
-    })
-  })
-});
+//           expect(res.statusCode).toBe(201);
+//         })
+//       })
+//     })
+//   })
+// });
 
 
 
-/***
- * 유효하지 않은 routineId
- * 온전하지 않은 request body
- * 없는 루틴 추가 시도
- * 루틴 2개 추가
- * 장바구니에 루틴 추가
- * 이미 장바구니에 존재중인 루틴 추가시도
- * 장바구니에 없는 루틴 추가 시도
- */
+// /***
+//  * 유효하지 않은 routineId
+//  * 온전하지 않은 request body
+//  * 없는 루틴 추가 시도
+//  * 루틴 2개 추가
+//  * 장바구니에 루틴 추가
+//  * 이미 장바구니에 존재중인 루틴 추가시도
+//  * 장바구니에 없는 루틴 추가 시도
+//  */
