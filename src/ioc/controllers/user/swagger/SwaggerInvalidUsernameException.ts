0c224129807f14ce73aabe201168fa5ea24fae9d
ksh <soohyeok8901@gmail.com>
2022-02-27 04:13:45 +0900

    Fix validateUsername swagger

--- src/ioc/controllers/user/swagger/SwaggerInvalidUsernameException.ts
+++ src/ioc/controllers/user/swagger/SwaggerInvalidUsernameException.ts
@@ -5,6 +5,6 @@ export class SwaggerInvalidUsernameException implements SwaggerServerException {
   @ApiProperty({ description: '메시지', example: '닉네임은 2자 이상 8자 이하여야 합니다' })
   public message: string;
 
-  @ApiProperty({ description: '에러종류', example: -1 })
+  @ApiProperty({ description: '에러종류', example: 1 })
   public errorCode: number;
 }
