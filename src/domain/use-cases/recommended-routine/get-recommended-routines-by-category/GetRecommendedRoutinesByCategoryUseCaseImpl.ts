732487ae6cc9693c90bfaa9f597fe694d64d40fe
ksh <soohyeok8901@gmail.com>
2022-03-17 02:34:57 +0900

    Fix get recommended routine(s) usecase

--- src/domain/use-cases/recommended-routine/get-recommended-routines-by-category/GetRecommendedRoutinesByCategoryUseCaseImpl.ts
+++ src/domain/use-cases/recommended-routine/get-recommended-routines-by-category/GetRecommendedRoutinesByCategoryUseCaseImpl.ts
@@ -3,6 +3,7 @@ import { Category } from '../../../common/enums/Category';
 import { RecommendedRoutineModel } from '../../../models/RecommendedRoutineModel';
 import { ImageProvider } from '../../../providers/ImageProvider';
 import { RecommendedRoutineRepository } from '../../../repositories/recommended-routine/RecommendedRoutineRepository';
+import { CommonRecommendedRoutineResponseDto } from '../common/CommonRecommendedRoutineResponseDto';
 import {
   CommonRecommendedRoutineService,
   HowToProveYouDidIt,
@@ -62,60 +63,48 @@ export class GetRecommendedRoutinesByCategoryUseCaseImpl
       ? recommendedRoutines[recommendedRoutines.length - 1]['_id']
       : null;
 
-    const mappedItems = await Promise.all(
-      recommendedRoutines.map(async (routine) => {
-        // image mapping
-        let thumbnailUrl;
+    const mappedItems: CommonRecommendedRoutineResponseDto[] =
+      await Promise.all(
+        recommendedRoutines.map(async (recommendedRoutine) => {
+          const thumbnailCDN = recommendedRoutine['thumbnail_id']
+            ? await this._imageProvider.requestImageToCDN(
+                recommendedRoutine['thumbnail_id'],
+              )
+            : null;
 
-        if (routine['thumbnail_id']) {
-          const thumbnailModel = this._imageProvider.mapDocumentToImageModel(
-            routine['thumbnail_id'],
-          );
+          const cardNewsCDN = recommendedRoutine['cardnews_id']
+            ? await this._imageProvider.requestImageToCDN(
+                recommendedRoutine['cardnews_id'],
+              )
+            : null;
 
-          thumbnailUrl = await this._imageProvider.requestImageToCloudfront(
-            thumbnailModel,
-          );
-        }
+          const howToProveYouDidIt: HowToProveYouDidIt =
+            CommonRecommendedRoutineService.getHowToProveByCategory(
+              recommendedRoutine['category'],
+            );
 
-        let cardnewsUrl;
-
-        if (routine['cardnews_id']) {
-          const cardnewsModel = this._imageProvider.mapDocumentToImageModel(
-            routine['cardnews_id'],
-          );
-
-          cardnewsUrl = await this._imageProvider.requestImageToCloudfront(
-            cardnewsModel,
-          );
-        }
-
-        const howToProveYouDidIt: HowToProveYouDidIt =
-          CommonRecommendedRoutineService.getHowToProveByCategory(
-            routine['category'],
-          );
-
-        return {
-          id: routine['_id'],
-          title: routine['title'],
-          category: routine['category'],
-          fixedFields: routine['fixedFields'],
-          hour: routine['hour'],
-          minute: routine['minute'],
-          days: routine['days'],
-          alarmVideoId: routine['alarmVideoId'],
-          contentVideoId: routine['contentVideoId'],
-          timerDuration: routine['timerDuration'],
-          price: routine['price'],
-          point: routine['point'],
-          exp: routine['exp'],
-          introduction: routine['introduction'],
-          thumbnail: thumbnailUrl,
-          cardnews: cardnewsUrl,
-          howToProveScript: howToProveYouDidIt.script,
-          howToProveImageUrl: howToProveYouDidIt.imageUrl,
-        };
-      }),
-    );
+          return {
+            id: recommendedRoutine['_id'],
+            title: recommendedRoutine['title'],
+            category: recommendedRoutine['category'],
+            fixedFields: recommendedRoutine['fixedFields'],
+            hour: recommendedRoutine['hour'],
+            minute: recommendedRoutine['minute'],
+            days: recommendedRoutine['days'],
+            alarmVideoId: recommendedRoutine['alarmVideoId'],
+            contentVideoId: recommendedRoutine['contentVideoId'],
+            timerDuration: recommendedRoutine['timerDuration'],
+            price: recommendedRoutine['price'],
+            point: recommendedRoutine['point'],
+            exp: recommendedRoutine['exp'],
+            introduction: recommendedRoutine['introduction'],
+            thumbnail: thumbnailCDN as string,
+            cardnews: cardNewsCDN as string[],
+            howToProveScript: howToProveYouDidIt.script,
+            howToProveImageUrl: howToProveYouDidIt.imageUrl,
+          };
+        }),
+      );
 
     return {
       hasMore,
