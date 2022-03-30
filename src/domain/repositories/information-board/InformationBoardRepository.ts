3b28c49c6a4fc92119c29b8d6bfbe7651174e53f
ksh <soohyeok8901@gmail.com>
2022-03-30 19:40:11 +0900

    Fix typo

--- src/domain/repositories/information-board/InformationBoardRepository.ts
+++ src/domain/repositories/information-board/InformationBoardRepository.ts
@@ -1,6 +1,6 @@
 import { InformationBoard } from '../../entities/InformationBoard';
-import { CreateBoardDto } from './dtos/createBoardDto';
-import { UpdateBoardDto } from './dtos/updateBoardDto';
+import { CreateBoardDto } from './dtos/CreateBoardDto';
+import { UpdateBoardDto } from './dtos/UpdateBoardDto';
 
 export abstract class InformationBoardRepository {
   abstract create(dto: CreateBoardDto): Promise<InformationBoard | null>;
