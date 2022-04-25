4c76073d289ef9e4705828fc56273100e868158b
oh-jinsu <ohjinsu@codersproduct.com>
2022-04-25 09:58:59 +0900

    Add the serial entity

---
+++ src/domain/entities/Serial.ts
@@ -0,0 +1,8 @@
+export class Serial {
+  public constructor(
+    public readonly id: string,
+    public readonly userId: string,
+    public readonly email: string,
+    public readonly serial: string,
+  ) {}
+}
