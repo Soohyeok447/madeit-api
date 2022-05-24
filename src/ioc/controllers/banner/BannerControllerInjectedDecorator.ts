cc222c8dcbc3398ed81804a3821108d51c2e8b2e
ksh <soohyeok8901@gmail.com>
2022-05-25 00:39:51 +0900

    Delete Authentication in get banner api

--- src/ioc/controllers/banner/BannerControllerInjectedDecorator.ts
+++ src/ioc/controllers/banner/BannerControllerInjectedDecorator.ts
@@ -1,13 +1,7 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import {
-  ApiBearerAuth,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { Controller, Get, Req } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { BannerController } from '../../../adapter/banner/BannerController';
-import { JwtAuthGuard } from '../../../adapter/common/guards/JwtAuthGuard.guard';
 import { GetBannerResponseDto } from '../../../domain/use-cases/banner/get-banner/dtos/GetBannerResponseDto';
 import { GetBannersResponseDto } from '../../../domain/use-cases/banner/get-banners/dtos/GetBannersResponseDto';
 
@@ -26,8 +20,11 @@ export class BannerControllerInjectedDecorator extends BannerController {
     - OPTIONAL -
     
     [Response]
-    200, 401
-    [에러코드]`,
+    200,404
+
+    [에러코드]
+    88 - 배너가 없음
+    `,
   })
   @ApiResponse({
     status: 200,
@@ -35,8 +32,6 @@ export class BannerControllerInjectedDecorator extends BannerController {
     불러온 배너`,
     type: GetBannerResponseDto,
   })
-  @ApiBearerAuth('JWT')
-  @UseGuards(JwtAuthGuard)
   @Get('/:id')
   public async getBanner(@Req() req: Request): Promise<GetBannerResponseDto> {
     return super.getBanner(req);
@@ -52,8 +47,11 @@ export class BannerControllerInjectedDecorator extends BannerController {
     - OPTIONAL -
     
     [Response]
-    200, 401
-    [에러코드]`,
+    200, 404
+
+    [에러코드]
+    88 - 배너가 1개도 없음
+    `,
   })
   @ApiResponse({
     status: 200,
@@ -62,8 +60,6 @@ export class BannerControllerInjectedDecorator extends BannerController {
     isArray: true,
     type: GetBannersResponseDto,
   })
-  @ApiBearerAuth('JWT')
-  @UseGuards(JwtAuthGuard)
   @Get('')
   public async getBanners(): Promise<GetBannersResponseDto[]> {
     return super.getBanners();
